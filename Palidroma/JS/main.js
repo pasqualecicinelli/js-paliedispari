b456260e6beefe35e10c577bcd8f89abdf264175
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-07-27 17:18:27 +0200

    feat decrease

--- Palidroma/JS/main.js
+++ Palidroma/JS/main.js
@@ -1,1 +1,14 @@
-console.log('js ok');
+const userWorld = prompt('Inserisci una parola: ');
+
+
+//function getPalindroma()
+
+
+
+for (let i = userWorld.length; i >= 0; i--) {
+    let reverse = '';
+    reverse += userWorld[i];
+
+    console.log(reverse);
+}
+console.log(userWorld);
