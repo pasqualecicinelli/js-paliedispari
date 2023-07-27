b3140a7a339f17cc5acd2d659b288908efa50ce2
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-07-27 15:59:05 +0200

    feat random and somma

--- Pari e Dispari/JS/main.js
+++ Pari e Dispari/JS/main.js
@@ -1,1 +1,52 @@
-console.log('js ok');
+const choose = prompt('Scegli: Pari o Dispari?');
+const userNumber = parseInt(prompt('Scegli un numero da 1 a 5'));
+
+//funzione random
+
+function getRandom(min = 1, max = 5, lastIncluded = true) {
+
+    if (lastIncluded) max++;
+
+    const randomNumber = Math.floor(Math.random() * (max - min) + min);
+
+    return randomNumber;
+}
+
+
+const randomComputer = getRandom();
+
+console.log(userNumber);
+console.log(randomComputer);
+
+// funzione somma
+
+function getSomma(firstNumber, secondNumber) {
+
+    let somma = firstNumber + secondNumber;
+    return somma;
+}
+
+const resultSomma = getSomma(userNumber, randomComputer);
+console.log(resultSomma);
+
+if (resultSomma % 2 == 0) {
+
+    console.log('L\'utente vince è pari: ' + resultSomma)
+
+
+} else {
+    console.log('Il risultato è dispari: ' + resultSomma)
+
+}
+
+/*if (choose.toUpperCase() == 'Pari') {
+    console.log('Vince l\'utente' + choose)
+} else {
+    console.log('Vince il Computer' + choose)
+    
+}*/
+
+
+
+
+
