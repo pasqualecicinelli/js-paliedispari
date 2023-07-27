4d25f610f12fd879b2493a20f01f560c750b0b9b
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-07-27 17:50:14 +0200

    feat function

--- Palidroma/JS/main.js
+++ Palidroma/JS/main.js
@@ -1,9 +1,21 @@
 const userWorld = prompt('Inserisci una parola: ');
 
+function getReverse(value = '') {
 
-//function getPalindroma()
+    for (let i = value.length; i > 0; value.length--) {
+        let reverse = '';
+        reverse += value[i];
+        return reverse
+    }
+
+}
+
+let reverse2 = getReverse(userWorld);
 
 
+console.log(reverse2)
+
+/*
 
 for (let i = userWorld.length; i >= 0; i--) {
     let reverse = '';
@@ -11,4 +23,14 @@ for (let i = userWorld.length; i >= 0; i--) {
 
     console.log(reverse);
 }
-console.log(userWorld);
+console.log(userWorld);*/
+
+/*for (i = 0; i < reverse.length; i++){
+
+    if (reverse[i] == userWorld[i]) {
+        
+        console.log('le parole sono palindrome');
+        console.log('utente ' userWorld + 'palindroma ' + reverse);
+    }
+}*/
+
